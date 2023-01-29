93a2ca86749d58b5672da89dce3040f285159a8b
msgerasyov <msgerasyov@yandex.ru>
2023-01-29 20:04:23 +0300

    Reformat training

--- train.py
+++ train.py
@@ -4,21 +4,21 @@ Heavily inspired by:
 https://albumentations.ai/docs/examples/pytorch_semantic_segmentation/
 https://github.com/optuna/optuna-examples/blob/main/pytorch/pytorch_lightning_simple.py
 """
-import os
-import pathlib
+import argparse
 import random
-from typing import Dict, Any
+from pathlib import Path
+from typing import Dict, Any, Optional, Iterable
 
 import mlflow
 import numpy as np
 import optuna
 import pytorch_lightning as pl
 import torch
-import yaml
 from optuna.integration import PyTorchLightningPruningCallback
+from pytorch_lightning import Callback, LightningModule, LightningDataModule
 from pytorch_lightning.callbacks import LearningRateMonitor
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-from pytorch_lightning.loggers import MLFlowLogger
+from pytorch_lightning.loggers import MLFlowLogger, LightningLoggerBase
 
 from utils import LightningDeepLabV3, OxfordIIITPetDataModule, get_transforms
 
@@ -27,48 +27,119 @@ def sample_params(trial: optuna.trial.Trial) -> Dict[str, Any]:
     lr = trial.suggest_float("lr", 1e-4, 5e-2, log=True)
     momentum = trial.suggest_float("momentum", 0.9, 0.99)
 
-    return {
-        "lr": lr,
-        "momentum": momentum
-    }
+    return {"lr": lr, "momentum": momentum}
 
 
-def objective(trial: optuna.trial.BaseTrial, config: Dict[str, Any]) -> float:
-    experiment = mlflow.get_experiment_by_name(config["mlflow"]["experiment_name"])
-    exp_id = experiment.experiment_id if experiment else mlflow.create_experiment(config["mlflow"]["experiment_name"])
-    run_name = config["mlflow"].get("run_name", str(trial.number))
+def train(
+    model: LightningModule,
+    datamodule: LightningDataModule,
+    n_epochs: int,
+    limit_train_batches: float = 1.0,
+    logger: LightningLoggerBase | Iterable[LightningLoggerBase] | bool = True,
+    callbacks: list[Callback] | Callback | None = None
+) -> Dict[str, Any]:
+    trainer = pl.Trainer(
+        logger=logger,
+        enable_checkpointing=False,
+        limit_train_batches=limit_train_batches,
+        max_epochs=n_epochs,
+        log_every_n_steps=10,
+        gpus=1 if torch.cuda.is_available() else None,
+        callbacks=callbacks
+    )
+    trainer.fit(model, datamodule=datamodule)
+
+    return trainer.callback_metrics
+
+
+def update_experiment(
+    configuration: Dict[str, Any],
+    n_epochs: int,
+    batch_size: int,
+    limit_train_batches: float = 1.0,
+    num_workers: int = 0,
+    data_dir: Optional[str] = None,
+    log_model: bool = False,
+    seed: int = 0,
+    experiment_name: str = "Default",
+    run_name: Optional[str] = None,
+    callbacks: list[Callback] | Callback | None = None
+) -> Dict[str, Any]:
+    experiment = mlflow.get_experiment_by_name(experiment_name)
+    if experiment:
+        exp_id = experiment.experiment_id
+    else:
+        exp_id = mlflow.create_experiment(experiment_name)
+    if data_dir is None:
+        data_dir = Path(__file__).parent.resolve()
 
     with mlflow.start_run(experiment_id=exp_id, run_name=run_name) as run:
-        # Use same code objective to reproduce the best model
-        params = sample_params(trial)
-        model = LightningDeepLabV3(lr=params["lr"], momentum=params["momentum"])
-        data_dir = config.get("data_dir", pathlib.Path(__file__).parent.resolve())
-        train_transforms, val_transforms, test_transforms = get_transforms()
-        datamodule = OxfordIIITPetDataModule(data_dir=data_dir, batch_size=config["batch_size"],
-                                             train_transforms=train_transforms, val_transforms=val_transforms,
-                                             test_transforms=test_transforms, download=True,
-                                             num_workers=config["n_workers"])
-        logger = MLFlowLogger(experiment_name=config["mlflow"]["experiment_name"], run_name=run_name)
+        logger = MLFlowLogger(
+            experiment_name=experiment_name,
+            run_name=run_name
+        )
         logger._run_id = run.info.run_id
-        callbacks = [PyTorchLightningPruningCallback(trial, monitor="val_BinaryJaccardIndex"),
-                     LearningRateMonitor(logging_interval="epoch")]
-        if "patience" in config:
-            callbacks.append(EarlyStopping(monitor="val_BinaryJaccardIndex", mode="max", patience=config["patience"]))
-        trainer = pl.Trainer(
+        logger.log_hyperparams(configuration)
+        model = LightningDeepLabV3(
+            lr=configuration["lr"],
+            momentum=configuration["momentum"]
+        )
+        train_transforms, val_transforms, test_transforms = get_transforms()
+        datamodule = OxfordIIITPetDataModule(
+            data_dir=data_dir,
+            batch_size=batch_size,
+            train_transforms=train_transforms,
+            val_transforms=val_transforms,
+            test_transforms=test_transforms,
+            download=True,
+            num_workers=num_workers,
+            seed=seed
+        )
+        metrics = train(
+            model,
+            datamodule,
+            n_epochs,
+            limit_train_batches=limit_train_batches,
             logger=logger,
-            enable_checkpointing=False,
-            limit_train_batches=config.get("limit_train_batches", 1.0),
-            max_epochs=config["epochs"],
-            log_every_n_steps=10,
-            gpus=1 if torch.cuda.is_available() else None,
             callbacks=callbacks
         )
-        trainer.logger.log_hyperparams(params)
-        trainer.fit(model, datamodule=datamodule)
-        if config["log_model"]:
+        if log_model:
             mlflow.pytorch.log_model(model, "model")
 
-        return trainer.callback_metrics["val_BinaryJaccardIndex"].item()
+        return metrics
+
+
+def objective(trial: optuna.trial.Trial, args: argparse.Namespace) -> float:
+    model_params = sample_params(trial)
+    callbacks = [
+        PyTorchLightningPruningCallback(
+            trial,
+            monitor="val_iou"
+        ),
+        LearningRateMonitor(logging_interval="epoch")
+    ]
+    if args.patience is not None:
+        callbacks.append(
+            EarlyStopping(
+                monitor="val_iou",
+                mode="max",
+                patience=args.patience
+            )
+        )
+    metrics = update_experiment(
+        model_params,
+        args.n_epochs,
+        args.batch_size,
+        limit_train_batches=args.limit_train_batches,
+        num_workers=args.num_workers,
+        data_dir=args.data_dir,
+        seed=args.seed,
+        experiment_name=args.experiment_name,
+        run_name=str(trial.number),
+        callbacks=callbacks
+    )
+
+    return metrics["val_iou"].item()
 
 
 def seed_rngs(seed: int) -> None:
@@ -78,11 +149,76 @@ def seed_rngs(seed: int) -> None:
 
 
 if __name__ == "__main__":
-    with open(os.path.join(pathlib.Path(__file__).parent.resolve(), "config.yml"), "r") as stream:
-        config = yaml.safe_load(stream)
-    seed_rngs(config["seed"])
-    sampler = optuna.samplers.TPESampler(seed=config["seed"])
-    pruner = optuna.pruners.MedianPruner(**config["optuna"]["pruner"])
-    study = optuna.create_study(direction="maximize", sampler=sampler, pruner=pruner)
-    study.optimize(lambda trial: objective(trial, config["tune"]), n_trials=config["optuna"]["n_trials"])
-    objective(study.best_trial, config["train"])
+    parser = argparse.ArgumentParser(
+        description='Train DeepLabV3 on Oxford-IIIT Pet Dataset')
+    parser.add_argument('--batch-size', default=8, type=int, help="Batch size")
+    parser.add_argument('--n-epochs', default=10, type=int,
+                        help='Number of training epochs')
+    parser.add_argument('--num-workers', default=0, type=int,
+                        help='Number of workers for dataloaders')
+    parser.add_argument('--data-dir', default=Path(__file__).parent.resolve(),
+                        type=str, help='Directory to save dataset')
+    parser.add_argument('--lr', default=7e-3, type=float, help='Learning rate')
+    parser.add_argument('--momentum', default=0.95, type=float,
+                        help='SGD momentum')
+    parser.add_argument('--patience', default=None, type=int,
+                        help="Patience for early stopping")
+    parser.add_argument("--optimize", action="store_true", default=False,
+                        help="Run hyperparameters search")
+    parser.add_argument('--n-trials', default=30, type=int,
+                        help='Number of trials')
+    parser.add_argument('--n-startup-trials', default=5, type=int,
+                        help='Number of startup trials with disabled pruning')
+    parser.add_argument('--n-warmup-steps', default=3, type=int,
+                        help='Number of epochs with disabled pruning')
+    parser.add_argument('--limit-train-batches', default=0.1, type=float,
+                        help='Reduce the training set size while tuning')
+    parser.add_argument('--seed', default=2023, type=int,
+                        help='Seed for random number generators')
+    parser.add_argument('--experiment-name', default="OxfordIIITPet", type=str,
+                        help='Name of mlflow experiment')
+    parser.add_argument('--run-name', default=None, type=str,
+                        help='Name of mlflow run')
+    args = parser.parse_args()
+    seed_rngs(args.seed)
+    if args.optimize:
+        sampler = optuna.samplers.TPESampler(seed=args.seed)
+        pruner = optuna.pruners.MedianPruner(
+            n_startup_trials=args.n_startup_trials,
+            n_warmup_steps=args.n_warmup_steps
+        )
+        study = optuna.create_study(
+            direction="maximize",
+            sampler=sampler,
+            pruner=pruner
+        )
+        study.optimize(
+            lambda trial: objective(trial, args),
+            n_trials=args.n_trials
+        )
+    else:
+        callbacks = [LearningRateMonitor(logging_interval="epoch")]
+        if args.patience is not None:
+            callbacks.append(
+                EarlyStopping(
+                    monitor="val_iou",
+                    mode="max",
+                    patience=args.patience
+                )
+            )
+        metrics = update_experiment(
+            configuration={
+                "lr": args.lr,
+                "momentum": args.momentum
+            },
+            n_epochs=args.n_epochs,
+            batch_size=args.batch_size,
+            limit_train_batches=1.0,
+            num_workers=args.num_workers,
+            data_dir=args.data_dir,
+            log_model=True,
+            seed=args.seed,
+            experiment_name=args.experiment_name,
+            run_name=args.run_name,
+            callbacks=callbacks
+        )
--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
-from typing import Any, Dict, Callable
-from typing import Optional
+from typing import Callable, Optional
 
 import albumentations as A
 import cv2
@@ -9,11 +8,11 @@ import torch
 import torch.nn as nn
 from albumentations.pytorch import ToTensorV2
 from sklearn.model_selection import train_test_split
-from torch import optim
+from torch.optim import SGD
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader, Subset
 from torchmetrics import MetricCollection
 from torchmetrics.classification import BinaryJaccardIndex, Dice
-from torchmetrics.functional import dice, jaccard_index
 from torchvision import datasets
 from torchvision.models import ResNet50_Weights
 from torchvision.models.segmentation import deeplabv3_resnet50
@@ -27,9 +26,10 @@ class LightningDeepLabV3(pl.LightningModule):
         weights = ResNet50_Weights.IMAGENET1K_V2
         self.model = deeplabv3_resnet50(num_classes=1, weights_backbone=weights)
         self.criterion = nn.BCEWithLogitsLoss()
-        metrics = MetricCollection([
-            BinaryJaccardIndex(), Dice()
-        ])
+        metrics = MetricCollection({
+            "iou": BinaryJaccardIndex(),
+            "dice": Dice()
+        })
         self.val_metrics = metrics.clone(prefix="val_")
         self.test_metrics = metrics.clone(prefix="test_")
 
@@ -63,13 +63,13 @@ class LightningDeepLabV3(pl.LightningModule):
         return output
 
     def configure_optimizers(self):
-        opt = optim.SGD(self.model.parameters(), lr=self.lr, momentum=self.momentum)
-        scheduler = optim.lr_scheduler.ReduceLROnPlateau(opt, mode="max", patience=2)
+        opt = SGD(self.model.parameters(), lr=self.lr, momentum=self.momentum)
+        scheduler = ReduceLROnPlateau(opt, mode="max", patience=2)
         return {
             "optimizer": opt,
             "lr_scheduler": {
                 "scheduler": scheduler,
-                "monitor": "val_BinaryJaccardIndex"
+                "monitor": "val_iou"
             }
         }
 
@@ -82,9 +82,19 @@ def preprocess_mask(mask):
 
 
 class OxfordIIITPetSegmentationDataset(datasets.OxfordIIITPet):
-    def __init__(self, root: str, split: str = "trainval", transforms: Optional[Callable] = None,
-                 download: bool = False):
-        super().__init__(root=root, split=split, target_types="segmentation", download=download)
+    def __init__(
+        self,
+        root: str,
+        split: str = "trainval",
+        transforms: Optional[Callable] = None,
+        download: bool = False
+    ):
+        super().__init__(
+            root=root,
+            split=split,
+            target_types="segmentation",
+            download=download
+        )
         self._transforms = transforms
 
     def __getitem__(self, index: int):
@@ -99,9 +109,17 @@ class OxfordIIITPetSegmentationDataset(datasets.OxfordIIITPet):
 
 
 class OxfordIIITPetDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir: str, batch_size: int, train_transforms: Optional[Callable] = None,
-                 val_transforms: Optional[Callable] = None, test_transforms: Optional[Callable] = None,
-                 download: bool = False, num_workers: int = 0, seed: int = 0):
+    def __init__(
+        self,
+        data_dir: str,
+        batch_size: int,
+        train_transforms: Optional[Callable] = None,
+        val_transforms: Optional[Callable] = None,
+        test_transforms: Optional[Callable] = None,
+        download: bool = False,
+        num_workers: int = 0,
+        seed: int = 0
+    ):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
@@ -113,28 +131,54 @@ class OxfordIIITPetDataModule(pl.LightningDataModule):
         self.seed = seed
 
     def setup(self, stage: Optional[str] = None) -> None:
-        self.pets_test = OxfordIIITPetSegmentationDataset(self.data_dir, split="test", transforms=self._test_transforms,
-                                                          download=self.download)
-        pets_train = OxfordIIITPetSegmentationDataset(self.data_dir, split="trainval",
-                                                      transforms=self._train_transforms, download=self.download)
-        pets_val = OxfordIIITPetSegmentationDataset(self.data_dir, split="trainval",
-                                                    transforms=self._val_transforms, download=self.download)
-        train_idx, val_idx = train_test_split(np.arange(len(pets_train)), test_size=0.15, random_state=self.seed)
-        self.pets_train, self.pets_val = Subset(pets_train, train_idx), Subset(pets_val, val_idx)
+        self.pets_test = OxfordIIITPetSegmentationDataset(
+            self.data_dir,
+            split="test",
+            transforms=self._test_transforms,
+            download=self.download
+        )
+        pets_train = OxfordIIITPetSegmentationDataset(
+            self.data_dir,
+            split="trainval",
+            transforms=self._train_transforms,
+            download=self.download
+        )
+        pets_val = OxfordIIITPetSegmentationDataset(
+            self.data_dir,
+            split="trainval",
+            transforms=self._val_transforms,
+            download=self.download
+        )
+        train_idx, val_idx = train_test_split(
+            np.arange(len(pets_train)),
+            test_size=0.25,
+            random_state=self.seed
+        )
+        self.pets_train = Subset(pets_train, train_idx)
+        self.pets_val = Subset(pets_val, val_idx)
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(
-            self.pets_train, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers
+            self.pets_train,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers
         )
 
     def val_dataloader(self) -> DataLoader:
         return DataLoader(
-            self.pets_val, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers
+            self.pets_val,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers
         )
 
     def test_dataloader(self) -> DataLoader:
         return DataLoader(
-            self.pets_test, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers
+            self.pets_test,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers
         )
 
 
@@ -143,8 +187,18 @@ def get_transforms():
         [
             A.PadIfNeeded(min_height=256, min_width=256),
             A.RandomCrop(256, 256),
-            A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5),
-            A.RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.5),
+            A.ShiftScaleRotate(
+                shift_limit=0.05,
+                scale_limit=0.05,
+                rotate_limit=15,
+                p=0.5
+            ),
+            A.RGBShift(
+                r_shift_limit=15,
+                g_shift_limit=15,
+                b_shift_limit=15,
+                p=0.5
+            ),
             A.RandomBrightnessContrast(p=0.5),
             A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ToTensorV2(),
@@ -162,7 +216,11 @@ def get_transforms():
 
     test_transform = A.Compose(
         [
-            A.PadIfNeeded(min_height=512, min_width=512, border_mode=cv2.BORDER_CONSTANT),
+            A.PadIfNeeded(
+                min_height=512,
+                min_width=512,
+                border_mode=cv2.BORDER_CONSTANT
+            ),
             A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ToTensorV2(),
         ]
